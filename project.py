8f89ead98bd55c7412fa11047753db44abf314e2
Hans Schuster <johannes.huster@web.de>
2017-08-09 15:43:46 +0200

    Add necessary DB imports

--- project.py
+++ project.py
@@ -1,10 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return 'Hello World'
+# Database imports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+# Accessing Database with ORM
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 
 if __name__ == '__main__':
     app.debug = True
