a46763840021b36e88d18d2c5b60678de36db1bd
Hans Schuster <johannes.huster@web.de>
2017-08-09 15:47:10 +0200

    Add response: restaurant's menu items

--- project.py
+++ project.py
@@ -13,6 +13,19 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 
+# Show menu items for specific restaurant
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name + '<br>'
+        output += i.price + '<br>'
+        output += i.description + '<br><br>'
+    return output
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
