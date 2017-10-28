8cfb7f4a5dff0d0da64dedfb8d28779d3e1b06ec
Hans Schuster <johannes.huster@web.de>
2017-10-28 10:50:03 +0200

    Update database-setup to include users

--- database_setup.py
+++ database_setup.py
@@ -7,14 +7,34 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    email = Column(String(80))
+    picture = Column(String(250))
+
+    # Serialize function: Send JSON objects in a serializable format
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+            'email': self.email,
+            'picture': self.picture
+        }
+
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-    # We added this serialize function to be able to send JSON objects in a
-    # serializable format
+    # Serialize function: Send JSON objects in a serializable format
     @property
     def serialize(self):
         return {
@@ -25,16 +45,17 @@ class Restaurant(Base):
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
-    name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-    # We added this serialize function to be able to send JSON objects in a
-    # serializable format
+    # Serialize function: Send JSON objects in a serializable format
     @property
     def serialize(self):
         return {
